e5504fb7a86cde08f6090b7445e851d45a28ffb5
Harsh Parakh <85981083+harsh-parakh22@users.noreply.github.com>
2025-11-03 16:11:25 +0530

    Add Firebase configuration file

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const FirebaseConfig = {
+  apiKey: "<key>",
+  authDomain: "crypto-creed-a0c31.firebaseapp.com",
+  projectId: "crypto-creed-a0c31",
+  storageBucket: "crypto-creed-a0c31.appspot.com",
+  messagingSenderId: "<id>",
+  appId: "<id>",
+  measurementId: "<id>"
+};
+
+export default FirebaseConfig;
