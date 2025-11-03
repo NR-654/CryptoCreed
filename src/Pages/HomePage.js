43202d06a44d2cd2055ca4632199dc6afb887eea
Neel Raval <neelrooney654@gmail.com>
2025-11-03 15:50:30 +0530

    Add Poster component to HomePage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import About from "../Components/About";
 import Header from "../Components/Header";
+import Poster from "../Components/Bg/Poster";
 import Contact from "../Components/Contact";
 import Features from "../Components/Features";
 import Footer from "../Components/Footer";
@@ -19,7 +20,7 @@ const HomePage = () => {
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <Header /> 
+      <Header /> <Poster />
       <br />
       <Features /> <br />
       <br /> <About /> <br /> <br /> <Contact /> <Footer />
