f337f4545e23261506e658884e168a4c1debfc0a
Harsh Parakh <85981083+harsh-parakh22@users.noreply.github.com>
2025-11-13 15:56:49 +0530

    Update Firebase config to use environment variables
    For deployment

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,11 +1,11 @@
 const FirebaseConfig = {
-  apiKey: "<key>",
-  authDomain: "crypto-creed-a0c31.firebaseapp.com",
-  projectId: "crypto-creed-a0c31",
-  storageBucket: "crypto-creed-a0c31.appspot.com",
-  messagingSenderId: "<id>",
-  appId: "<id>",
-  measurementId: "<id>"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 export default FirebaseConfig;
